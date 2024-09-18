790d211f9dfa5c61f25fe5680b1008e33bf199dc
luvuyotafeni <luvuyotafeni012@gmail.com>
2024-09-18 19:07:30 +0200

    added the sections on the home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,22 +2,24 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='container'>
-        <div className='row'>
-            <div>
+    <div>
+        <section>
+            <div className='row'>
                 <div>
-                    <h3>Luxurous Digitals</h3>
-                    <h5>Shop with us shop digitally</h5>
+                    <div>
+                        <h3>Luxurous Digitals</h3>
+                        <h5>Shop with us shop digitally</h5>
+                    </div>
+                    <div>
+                        <button>Shop all</button>
+                        <button>Contact Us</button>
+                    </div>
                 </div>
                 <div>
-                    <button>Shop all</button>
-                    <button>Contact Us</button>
-                </div>
-            </div>
-            <div>
 
+                </div>
             </div>
-        </div>
+        </section>
     </div>
   )
 }
