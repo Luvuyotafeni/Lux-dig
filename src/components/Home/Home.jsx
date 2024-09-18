04870fa5e9dabd05c6412139df446ea98cbe179e
luvuyotafeni <luvuyotafeni012@gmail.com>
2024-09-18 18:55:36 +0200

    added the home assets and also few

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,7 +2,23 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='container'>
+        <div className='row'>
+            <div>
+                <div>
+                    <h3>Luxurous Digitals</h3>
+                    <h5>Shop with us shop digitally</h5>
+                </div>
+                <div>
+                    <button>Shop all</button>
+                    <button>Contact Us</button>
+                </div>
+            </div>
+            <div>
+
+            </div>
+        </div>
+    </div>
   )
 }
 
