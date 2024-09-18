5c49852325da0258667cc0b30d866dbc468cf37f
luvuyotafeni <luvuyotafeni012@gmail.com>
2024-09-18 18:03:53 +0200

    added the login modal

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Navbar from './components/Header/Header';
-import Home from './components/Home/Home';
-import About from './components/About/About';
-import Products from './components/Products/Products';
-import Promotions from './components/Promotions/Promotions';
-import Enquire from './components/Enquire/Enquire';
-import Cart from './components/Cart/Cart';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Navbar from './components/Header/Header'
+import Home from './components/Home/Home'
+import About from './components/About/About'
+import Products from './components/Products/Products'
+import Promotions from './components/Promotions/Promotions'
+import Enquire from './components/Enquire/Enquire'
+import Cart from './components/Cart/Cart'
 
 function App() {
   return (
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 import Cart from '../Cart/Cart'
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom'
+import LoginModal from '../LoginModal/LoginModal'
+import './Header.css'
 
 const Header = () => {
 
-    // const [showLogin, setShowLogin] = useState(false);
+    const [showLogin, setShowLogin] = useState(false);
 
-    // const handleLoginClick = () => {
-    //   setShowLogin(true);
-    // };
+    const handleLoginClick = () => {
+      setShowLogin(true);
+    };
   
-    // const handleCloseModal = () => {
-    //   setShowLogin(false);
-    // };
+    const handleCloseModal = () => {
+      setShowLogin(false);
+    };
   return (
     <nav className="navbar">
     <ul>
@@ -21,10 +23,10 @@ const Header = () => {
       <li><Link to="/products">Products</Link></li>
       <li><Link to="/promotions">Promotions</Link></li>
       <li><Link to="/enquire">Enquire</Link></li>
-      {/* <li><button onClick={handleLoginClick}>Login</button></li> */}
+      <li><button onClick={handleLoginClick}>Login</button></li>
       <li><Cart /></li>
     </ul>
-    {/* {showLogin && <LoginModal onClose={handleCloseModal} />} */}
+    {showLogin && <LoginModal onClose={handleCloseModal} />}
   </nav>
 );
 };
