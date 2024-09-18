7c6be637abde32f88be55b882ed0c320a094f5b7
luvuyotafeni <luvuyotafeni012@gmail.com>
2024-09-18 17:31:56 +0200

    added the header component and the routes on the app.js

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import Cart from '../Cart/Cart'
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+
+    const [showLogin, setShowLogin] = useState(false);
+
+    const handleLoginClick = () => {
+      setShowLogin(true);
+    };
+  
+    const handleCloseModal = () => {
+      setShowLogin(false);
+    };
+  return (
+    <nav className="navbar">
+    <ul>
+      <li><Link to="/">Home</Link></li>
+      <li><Link to="/about">About</Link></li>
+      <li><Link to="/products">Products</Link></li>
+      <li><Link to="/promotions">Promotions</Link></li>
+      <li><Link to="/enquire">Enquire</Link></li>
+      <li><button onClick={handleLoginClick}>Login</button></li>
+      <li><Cart /></li>
+    </ul>
+    {showLogin && <LoginModal onClose={handleCloseModal} />}
+  </nav>
+);
+};
+export default Header
