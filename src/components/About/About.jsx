a52b74f1d5c9067cd759e24472fcf8a161e4db94
luvuyotafeni <luvuyotafeni012@gmail.com>
2024-09-19 08:42:07 +0200

    about cards

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -2,7 +2,38 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div>
+      <section>
+        <div className='container'>
+          <div className='row'>
+            <div className='col-md-4'>
+              <div className='card middle-card'>
+                <img src=''/>
+                <div className='card-body'>
+                  <p>24hrs</p>
+                </div>
+              </div>
+            </div>
+            <div className='col-md-4'>
+              <div className='card middle-card'>
+                <img src=''/>
+                <div className='card-body'>
+                  <p>Nationwide Delivery</p>
+                </div>
+              </div>
+            </div>
+            <div className='col-md-4'>
+              <div className='card middle-card'>
+                <img src=''/>
+                <div className='card-body'>
+                  <p>Fully digital</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
   )
 }
 
