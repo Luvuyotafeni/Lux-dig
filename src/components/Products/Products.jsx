93d43c08d1246049258a3aece425e569f097d7af
luvuyotafeni <luvuyotafeni012@gmail.com>
2024-09-25 11:47:22 +0200

    added the products on the products page but still about to debug the code

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, {useState} from 'react'
+import data from './Produts-api'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min'
 
 const Products = () => {
+
+  const[selectedProduct, setSelectedProduct] = useState(null);
+
+  const handleShowModal = (product) => {
+    setSelectedProduct(product);
+  };
   return (
-    <div>Products</div>
+    <div className='container'>
+      <div className='row'>
+        {data.map((item, index) => (
+          item.models.map(model, idx) => (
+            <div className='col-md-3 mb-4' key={`${index}-${idx}`}>
+              <div className='card'>
+                <img src=''/>
+              </div>
+            </div>
+          )
+        ))}
+      </div>
+    </div>
   )
 }
 
