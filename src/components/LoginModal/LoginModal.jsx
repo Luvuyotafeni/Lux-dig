b0cebaa61fb6f72085fbda471cd1d40baf5f494c
luvuyotafeni <luvuyotafeni012@gmail.com>
2024-09-27 10:19:46 +0200

    added the structure for the log in component

--- src/components/LoginModal/LoginModal.jsx
+++ src/components/LoginModal/LoginModal.jsx
@@ -3,7 +3,35 @@ import './LoginModal.css'
 
 const LoginModal = () => {
   return (
-    <div>LoginModal</div>
+    <>
+      {/* LoginModal */}
+      <div className='modal fade show' role='dialog' style={{display:'block', backgroundColor: 'rgba9(0, 0, 0, 0.5)'}}>
+        <div className='modal-dialog modal-dialog-centered'>
+          <div className='modal-content'>
+            <div className='modal-header'>
+              <h5 className='modal-title'>Log In</h5>
+              <button type='button' className='close' aria-label='close'>
+                <span aria-hidden='true'>&times;</span>
+              </button>            
+            </div>
+            <div className='modal-body'>
+              <form>
+                <div className='form-group'>
+                  <label htmlfor='email'>Email address</label>
+                  <input type='email' className='form-control' id='email' placeholder='e.g lux@gmail.com'></input>
+                </div>
+                <div className='form-group'>
+                  <label htmlFor='password'>Enter Password</label>
+                  <input type='password' className='password' id='password' placeholder='Enter password'/>
+                </div>
+                <button type='submit' className='btn btn-primary btn-block'>Log In</button>
+              </form>
+            </div>
+          </div>
+          
+        </div>
+      </div>
+    </>
   )
 }
 
