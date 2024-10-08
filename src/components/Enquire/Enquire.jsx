fc7e5bc3186ed6e22f1a3c24285ee36e890590ae
luvuyotafeni <luvuyotafeni012@gmail.com>
2024-10-08 18:03:27 +0200

    added the contact form

--- src/components/Enquire/Enquire.jsx
+++ src/components/Enquire/Enquire.jsx
@@ -30,6 +30,38 @@ const Enquire = () => {
           </div>
         </div>
       </div>
+      <div className="row mt-5">
+        <div className="col-md-8 offset-md-2">
+          <div className="card">
+            <div className="card-body">
+              <h5 className="card-title">Contact Us</h5>
+              <form>
+                <div className="form-group">
+                  <label htmlFor="name">Name</label>
+                  <input type="text" className="form-control" id="name" placeholder="Enter your name" />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="surname">Surname</label>
+                  <input type="text" className="form-control" id="surname" placeholder="Enter your surname" />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="email">Email</label>
+                  <input type="email" className="form-control" id="email" placeholder="Enter your email" />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="subject">Subject</label>
+                  <input type="text" className="form-control" id="subject" placeholder="Subject" />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="message">Message</label>
+                  <textarea className="form-control" id="message" rows="5" placeholder="Your message"></textarea>
+                </div>
+                <button type="submit" className="btn btn-primary mt-3">Submit</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
