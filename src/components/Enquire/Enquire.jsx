09f25465d5f68037b02945afecf2e874605bd9ec
luvuyotafeni <luvuyotafeni012@gmail.com>
2024-10-08 17:47:42 +0200

    added the contact details division

--- src/components/Enquire/Enquire.jsx
+++ src/components/Enquire/Enquire.jsx
@@ -2,7 +2,35 @@ import React from 'react'
 
 const Enquire = () => {
   return (
-    <div>Enquire</div>
+    <div className="container mt-5">
+      {/* Contact Details Section */}
+      <div className="row">
+        <div className="col-md-4">
+          <div className="card text-center">
+            <div className="card-body">
+              <h5 className="card-title">Phone Number</h5>
+              <p className="card-text">+27 123 456 789</p>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-4">
+          <div className="card text-center">
+            <div className="card-body">
+              <h5 className="card-title">Email</h5>
+              <p className="card-text">luvuyotafeni012@gmail.com</p>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-4">
+          <div className="card text-center">
+            <div className="card-body">
+              <h5 className="card-title">Address</h5>
+              <p className="card-text">123 Street, City, Country</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
