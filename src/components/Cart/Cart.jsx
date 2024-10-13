ff128bea53e4c5826fe940413896869fbe55b324
luvuyotafeni <luvuyotafeni012@gmail.com>
2024-10-13 11:00:44 +0200

    added the code for deleting the cart items, first implementation

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -4,7 +4,33 @@ const Cart = ({cartItems, handleDeleteFromCart}) => {
   return (
     <div className='container mt-4'>
       <h2>Your Cart</h2>
-      {cartItems.length === 0 ? }
+      {cartItems.length === 0 ? (
+        <h1>Your cart is empty</h1>
+      ) : (
+        <table className='table table-striped'>
+          <thead>
+            <tr>
+              <th>Image</th>
+              <th>Version</th>
+              <th>Price</th>
+              <th>Description</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {cartItems.map((item, index) => (
+              <tr key={index}>
+                <td>
+                  <img src={item.image} alt={item.version} style={{width: '50px'}}/>
+                </td>
+                <td>{item.version}</td>
+                <td>{item.desc}</td>
+                <td><button className='btn btn-danger' onClick={() => handleDeleteFromCart(index)}> delete</button></td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      ) }
 
     </div>
   )
